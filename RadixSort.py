9c0620eeaf03cb7450069ac39e7443212e596601
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-09-14 07:12:56 +0530

    RadixSortPt1

---
+++ RadixSort.py
@@ -0,0 +1,24 @@
+
+# ! Radix Sort
+
+# * Based on Counting Sort but more complicated
+
+# * It is used for sorting large data sets and can even sort strings and huge integers
+
+# * It is better than Quick and Merge Sort in the Above Case
+
+# * Time Complexity is O(n . k) where 
+ 
+# * n is the no. values present in the array 
+ 
+# * and k is the no. of digits in each value (like 132 has 3 digits, 12345 has 5 digits, 7658904 has 7 digits)
+
+# * The Best Case Time Complexity is O(n) cause k can be 1
+
+# * The Worst Case Time Complexity is O(n^2)
+
+# * The Average Time Complexity is O(n . log n) cause n can be ridiculous high no.
+
+# * Radix Sort requires another Sorting Algorithm which should be a Stable Sorting Algorithm for sorting each layer of digit
+
+# * Refer Readme for defination of Stable Sorting Algorithm
